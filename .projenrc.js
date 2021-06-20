6281c378875ee52044f8fb7bc94df1f40a3cfb16
mats16 <mats.kazuki@gmail.com>
2021-06-20 17:37:32 +0900

    initial commit

--- .projenrc.js
+++ .projenrc.js
@@ -1,6 +1,6 @@
 const { AwsCdkTypeScriptApp } = require('projen');
 const project = new AwsCdkTypeScriptApp({
-  cdkVersion: '1.95.2',
+  cdkVersion: '1.109.0',
   defaultReleaseBranch: 'main',
   name: 'bitwarden-on-aws',
 
