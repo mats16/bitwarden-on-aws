0024f98a4cdce9f3454035253ca64a838fd17095
mats16 <mats.kazuki@gmail.com>
2021-06-27 19:12:08 +0900

    update waf

--- src/bitwarden-waf-stack.ts
+++ src/bitwarden-waf-stack.ts
@@ -10,29 +10,8 @@ export class BitwardenWafStack extends cdk.Stack {
       scope: 'CLOUDFRONT',
       rules: [
         {
-          name: 'AWS-AWSManagedRulesBotControlRuleSet',
-          priority: 0,
-          statement: {
-            managedRuleGroupStatement: {
-              vendorName: 'AWS',
-              name: 'AWSManagedRulesBotControlRuleSet',
-              excludedRules: [
-                { name: 'SignalNonBrowserUserAgent' }, // for mobile app
-              ],
-            },
-          },
-          overrideAction: {
-            none: {},
-          },
-          visibilityConfig: {
-            sampledRequestsEnabled: true,
-            cloudWatchMetricsEnabled: true,
-            metricName: 'AWS-AWSManagedRulesBotControlRuleSet',
-          },
-        },
-        {
           name: 'AWS-AWSManagedRulesAmazonIpReputationList',
-          priority: 1,
+          priority: 0,
           statement: {
             managedRuleGroupStatement: {
               vendorName: 'AWS',
@@ -49,31 +28,8 @@ export class BitwardenWafStack extends cdk.Stack {
           },
         },
         {
-          name: 'AWS-AWSManagedRulesCommonRuleSet',
-          priority: 2,
-          statement: {
-            managedRuleGroupStatement: {
-              vendorName: 'AWS',
-              name: 'AWSManagedRulesCommonRuleSet',
-              excludedRules: [
-                { name: 'GenericRFI_QUERYARGUMENTS' }, // for FIDO2 WebAuthn
-                { name: 'SizeRestrictions_BODY' }, // for Bitwarden Send (file)
-                { name: 'CrossSiteScripting_BODY' }, // for Bitwarden Send (file)
-              ],
-            },
-          },
-          overrideAction: {
-            none: {},
-          },
-          visibilityConfig: {
-            sampledRequestsEnabled: true,
-            cloudWatchMetricsEnabled: true,
-            metricName: 'AWS-AWSManagedRulesCommonRuleSet',
-          },
-        },
-        {
           name: 'AWS-AWSManagedRulesKnownBadInputsRuleSet',
-          priority: 3,
+          priority: 1,
           statement: {
             managedRuleGroupStatement: {
               vendorName: 'AWS',
@@ -91,7 +47,7 @@ export class BitwardenWafStack extends cdk.Stack {
         },
         {
           name: 'AWS-AWSManagedRulesSQLiRuleSet',
-          priority: 4,
+          priority: 2,
           statement: {
             managedRuleGroupStatement: {
               vendorName: 'AWS',
@@ -105,6 +61,239 @@ export class BitwardenWafStack extends cdk.Stack {
             sampledRequestsEnabled: true,
             cloudWatchMetricsEnabled: true,
             metricName: 'AWS-AWSManagedRulesSQLiRuleSet',
+          },
+        },
+        {
+          name: 'Bitwarden-GetIconsRule',
+          priority: 3,
+          statement: {
+            andStatement: {
+              statements: [
+                {
+                  byteMatchStatement: {
+                    fieldToMatch: { uriPath: {} },
+                    positionalConstraint: 'STARTS_WITH',
+                    searchString: '/icons/',
+                    textTransformations: [{
+                      priority: 0, type: 'NONE'
+                    }]
+                    
+                  }
+                },
+                {
+                  byteMatchStatement: {
+                    fieldToMatch: { method: {} },
+                    positionalConstraint: 'EXACTLY',
+                    searchString: 'GET',
+                    textTransformations: [{
+                      priority: 0, type: 'NONE'
+                    }]
+                  }
+                }
+              ]
+            }
+          },
+          action: { allow: {} },
+          visibilityConfig: {
+            sampledRequestsEnabled: false,
+            cloudWatchMetricsEnabled: false,
+            metricName: 'Bitwarden-GetIconsRule',
+          },
+        },
+        {
+          name: 'Bitwarden-PostSendFileRule',
+          priority: 4,
+          statement: {
+            andStatement: {
+              statements: [
+                {
+                  byteMatchStatement: {
+                    fieldToMatch: { uriPath: {} },
+                    positionalConstraint: 'STARTS_WITH',
+                    searchString: '/api/sends/',
+                    textTransformations: [{
+                      priority: 0, type: 'NONE'
+                    }]
+                  }
+                },
+                {
+                  byteMatchStatement: {
+                    fieldToMatch: { method: {} },
+                    positionalConstraint: 'EXACTLY',
+                    searchString: 'POST',
+                    textTransformations: [{
+                      priority: 0, type: 'NONE'
+                    }]
+                  }
+                },
+              ]
+            }
+          },
+          action: { allow: {} },
+          visibilityConfig: {
+            sampledRequestsEnabled: false,
+            cloudWatchMetricsEnabled: false,
+            metricName: 'Bitwarden-PostSendFileRule',
+          },
+        },
+        {
+          name: 'Bitwarden-GetSendFileRule',
+          priority: 5,
+          statement: {
+            andStatement: {
+              statements: [
+                {
+                  byteMatchStatement: {
+                    fieldToMatch: { uriPath: {} },
+                    positionalConstraint: 'STARTS_WITH',
+                    searchString: '/attachments/send/',
+                    textTransformations: [{
+                      priority: 0, type: 'NONE'
+                    }]
+                  }
+                },
+                {
+                  byteMatchStatement: {
+                    fieldToMatch: { method: {} },
+                    positionalConstraint: 'EXACTLY',
+                    searchString: 'GET',
+                    textTransformations: [{
+                      priority: 0, type: 'NONE'
+                    }]
+                  }
+                },
+              ]
+            }
+          },
+          action: { allow: {} },
+          visibilityConfig: {
+            sampledRequestsEnabled: false,
+            cloudWatchMetricsEnabled: false,
+            metricName: 'Bitwarden-GetSendFileRule',
+          },
+        },
+        {
+          name: 'Bitwarden-WebAuthnRule',
+          priority: 6,
+          statement: {
+            andStatement: {
+              statements: [
+                {
+                  byteMatchStatement: {
+                    fieldToMatch: { uriPath: {} },
+                    positionalConstraint: 'STARTS_WITH',
+                    searchString: '/webauthn-connector.html',
+                    textTransformations: [{
+                      priority: 0, type: 'NONE'
+                    }]
+                    
+                  }
+                },
+                {
+                  byteMatchStatement: {
+                    fieldToMatch: { method: {} },
+                    positionalConstraint: 'EXACTLY',
+                    searchString: 'GET',
+                    textTransformations: [{
+                      priority: 0, type: 'NONE'
+                    }]
+                    
+                  }
+                },
+                {
+                  byteMatchStatement: {
+                    fieldToMatch: { singleHeader: { name: 'sec-fetch-mode' } },
+                    positionalConstraint: 'EXACTLY',
+                    searchString: 'navigate',
+                    textTransformations: [{
+                      priority: 0, type: 'NONE'
+                    }] 
+                  }
+                },
+                {
+                  byteMatchStatement: {
+                    fieldToMatch: { singleHeader: { name: 'sec-fetch-site' } },
+                    positionalConstraint: 'EXACTLY',
+                    searchString: 'same-origin',
+                    textTransformations: [{
+                      priority: 0, type: 'NONE'
+                    }] 
+                  }
+                },
+                {
+                  byteMatchStatement: {
+                    fieldToMatch: { singleHeader: { name: 'sec-fetch-dest' } },
+                    positionalConstraint: 'EXACTLY',
+                    searchString: 'iframe',
+                    textTransformations: [{
+                      priority: 0, type: 'NONE'
+                    }] 
+                  }
+                },
+              ]
+            }
+          },
+          action: { allow: {} },
+          visibilityConfig: {
+            sampledRequestsEnabled: false,
+            cloudWatchMetricsEnabled: false,
+            metricName: 'Bitwarden-WebAuthnRule',
+          },
+        },
+        {
+          name: 'AWS-AWSManagedRulesCommonRuleSet',
+          priority: 7,
+          statement: {
+            managedRuleGroupStatement: {
+              vendorName: 'AWS',
+              name: 'AWSManagedRulesCommonRuleSet',
+            },
+          },
+          overrideAction: {
+            none: {},
+          },
+          visibilityConfig: {
+            sampledRequestsEnabled: true,
+            cloudWatchMetricsEnabled: true,
+            metricName: 'AWS-AWSManagedRulesCommonRuleSet',
+          },
+        },
+        {
+          name: 'Bitwarden-MobileAppUserAgentRule',
+          priority: 8,
+          statement:                 {
+            byteMatchStatement: {
+              fieldToMatch: { singleHeader: { name: 'user-agent' } },
+              positionalConstraint: 'STARTS_WITH',
+              searchString: 'Bitwarden_Mobile',
+              textTransformations: [{
+                priority: 0, type: 'NONE'
+              }] 
+            }
+          },
+          action: { allow: {} },
+          visibilityConfig: {
+            sampledRequestsEnabled: false,
+            cloudWatchMetricsEnabled: false,
+            metricName: 'Bitwarden-MobileAppUserAgentRule',
+          },
+        },
+        {
+          name: 'AWS-AWSManagedRulesBotControlRuleSet',
+          priority: 9,
+          statement: {
+            managedRuleGroupStatement: {
+              vendorName: 'AWS',
+              name: 'AWSManagedRulesBotControlRuleSet',
+            },
+          },
+          overrideAction: {
+            none: {},
+          },
+          visibilityConfig: {
+            sampledRequestsEnabled: true,
+            cloudWatchMetricsEnabled: true,
+            metricName: 'AWS-AWSManagedRulesBotControlRuleSet',
           },
         },
       ],
